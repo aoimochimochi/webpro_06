94822cee6ec3e13a983cc1c64ab4214c611d95ba
Ao <aoi@Ks-MacBook-Air.local>
2024-11-04 12:40:09 +0900

    Initial commit

--- app5.js
+++ app5.js
@@ -32,23 +32,34 @@ app.get("/janken", (req, res) => {
   let win = Number( req.query.win );
   let total = Number( req.query.total );
   console.log( {hand, win, total});
+
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
+
+  let judgement = '';
+  if ((hand === 'グー' && cpu === 'チョキ') ||
+      (hand === 'チョキ' && cpu === 'パー') ||
+      (hand === 'パー' && cpu === 'グー')) {
+    judgement = '勝ち';
+    win += 1;
+  } else if (hand === cpu) {
+    judgement = '引き分け';
+  } else {
+    judgement = '負け';
+  }
+  
   total += 1;
+
   const display = {
     your: hand,
     cpu: cpu,
     judgement: judgement,
     win: win,
     total: total
-  }
+  };
   res.render( 'janken', display );
 });
 
